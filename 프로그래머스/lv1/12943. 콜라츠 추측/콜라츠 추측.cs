172ddf102e7dbc9a2a0e80aac5c1c6f780526e2f
HYEONJI LEE <77392694+szlovelee@users.noreply.github.com>
2023-08-28 09:56:49 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.17 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.cs
@@ -0,0 +1,14 @@
+public class Solution 
+{
+    public int solution(long num) 
+    {
+        int answer = 0;
+        while (num != 1)
+        {
+            if (num % 2 == 0) num /= 2;
+            else num = (num * 3) + 1;
+            ++answer;
+        }
+        return (answer <= 500)? answer : -1 ;
+    }
+}
