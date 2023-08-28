9ba42f66c167b776d8c792a2e3751b0a5f826989
HYEONJI LEE <77392694+szlovelee@users.noreply.github.com>
2023-08-28 10:36:43 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.46 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution 
+{
+    public int solution(int[] numbers) 
+    {
+        int answer = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            if (Array.IndexOf(numbers, i) == -1) answer += i;
+        }
+        return answer;
+    }
+}
